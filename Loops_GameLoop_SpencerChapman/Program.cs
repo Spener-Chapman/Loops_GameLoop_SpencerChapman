93be76e24d13e2e9c1d0d0a4b6c6c2e661f8f568
Spener-Chapman <spencerchap@hotmail.com>
2021-10-19 10:21:44 -0300

    Initial commit

---
+++ Loops_GameLoop_SpencerChapman/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops_GameLoop_SpencerChapman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool gameOver = false;
+
+            int xLine = 0; // x 0 to 119
+            int yLine = 0; // 0 to infinite
+
+            void PlayerDraw(int x, int y)
+            {
+                Console.SetCursorPosition(xLine, yLine);
+                
+            }
+            
+            void PlayerUpdate()
+            {
+                
+                ConsoleKeyInfo readKeyInput = Console.ReadKey(true);
+                
+                if (readKeyInput.Key == ConsoleKey.W)
+                {
+                    yLine = yLine - 1;
+                }
+                if (readKeyInput.Key == ConsoleKey.A)
+                {
+                    xLine = xLine - 1;
+                }
+                if (readKeyInput.Key == ConsoleKey.S)
+                {
+                    yLine = yLine + 1;
+                }
+                if (readKeyInput.Key == ConsoleKey.D)
+                {
+                    xLine = xLine + 1;
+                }
+                if (yLine >= 30)                 // start of range checking
+                {
+                    yLine = 30;
+                }
+                if (xLine <= 0)
+                {
+                    xLine = 0;
+                }
+                if (xLine >= 119)
+                {
+                    xLine = 119;
+                }
+                if (yLine <= 0)
+                {
+                    yLine = 0;
+                }
+                if (readKeyInput.Key == ConsoleKey.Escape)
+                {
+                    gameOver = true; // ends the game
+                }
+            }
+
+            while (gameOver == false) // the loop
+            {
+                PlayerDraw(xLine, yLine);
+                PlayerUpdate();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Loops_GameLoop_SpencerChapman
+ 
